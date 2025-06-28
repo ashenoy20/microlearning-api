3db692833064591a73bc31de679c8ef342d369f9
Ashish Shenoy <ashishshenoy9@gmail.com>
2025-06-28 14:30:03 -0400

    fix lint

--- app/db/database.py
+++ app/db/database.py
@@ -5,12 +5,17 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from beanie import Document, init_beanie
 from pydantic import Field
 
-MONGODB_URI = os.getenv("MONGODB_URI", "mongodb://microlearn:microlearn@mongodb:27017/microlearn?authSource=admin")
+MONGODB_URI = os.getenv(
+    "MONGODB_URI",
+    "mongodb://microlearn:microlearn@mongodb:27017/microlearn?authSource=admin",
+)
+
 
 async def init_db():
     # Create Motor client
     client = AsyncIOMotorClient(MONGODB_URI)
     await init_beanie(database=client.db_name, document_models=[User, Card, Deck])
+
 
 class User(Document):
     username: str
@@ -19,12 +24,14 @@ class User(Document):
     created_at: datetime = Field(default_factory=datetime.now)
     updated_at: datetime
 
+
 class Card(Document):
     front: str
     back: str
     user: Optional[User] = None
     created_at: datetime = Field(default_factory=datetime.now)
     updated_at: datetime
+
 
 class Deck(Document):
     title: str
