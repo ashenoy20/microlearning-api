7510caadeedcb9f1eead6cccaf7aa9468ca14acf
Arslan Tariq <99298951+ArslanTariq575@users.noreply.github.com>
2025-06-29 12:23:11 -0400

    Adding Test Folder in Backend Folder

---
+++ backend/tests/test_main.py
@@ -0,0 +1,13 @@
+import pytest
+from httpx import AsyncClient
+from backend.main import app
+from httpx import ASGITransport
+
+
+@pytest.mark.asyncio
+async def test_read_root():
+    transport = ASGITransport(app=app)
+    async with AsyncClient(transport=transport, base_url="http://test") as ac:
+        response = await ac.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
